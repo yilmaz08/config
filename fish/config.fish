6368623a2fd7e32c028c7d8e8d54a4e60c616bf1
Abdürrahim YILMAZ <84680978+yilmaz08@users.noreply.github.com>
2024-08-14 15:20:13 +0300

    move aliases to another file

--- fish/config.fish
+++ fish/config.fish
@@ -1,59 +1,3 @@
-alias py="python3"
-alias pong="ping archlinux.org"
-alias trm="trash-put"
-
-alias adamimprojevarmiya="cd ~/Projects/"
-
-# clear
-alias c="clear"
-alias clr="clear"
-alias cls="clear"
-
-# fetch
-alias ff="fastfetch"
-alias fetch="ff -c examples/13"
-
-# ls
-alias ls="/usr/bin/exa"
-alias l="ls -l"
-alias la="ls -la"
-
-alias r="ranger"
-alias e="nvim"
-alias se="sudo nvim"
-
-# aur helper
-alias aur="yay"
-alias S="aur -S"
-alias Ss="aur -Ss"
-alias Si="aur -Si"
-alias Syu="aur -Syu"
-alias Syyu="aur -Syyu"
-alias R="aur -R"
-alias Rcns="aur -Rcns"
-alias Q="aur -Q"
-alias Qe="aur -Qe"
-alias Su="aur -Su"
-alias Fy="aur -Fy"
-alias F="aur -F"
-
-# systemctl
-alias sstart="sudo systemctl start"
-alias srestart="sudo systemctl restart"
-alias sreload="sudo systemctl reload"
-alias sstop="sudo systemctl stop"
-alias senable="sudo systemctl enable"
-alias sdisable="sudo systemctl disable"
-alias sstatus="sudo systemctl status"
-
-# bluetoothctl
-alias btrust="bluetoothctl trust"
-alias bpair="bluetoothctl pair"
-alias bconn="bluetoothctl connect"
-alias bdisconn="bluetoothctl disconnect"
-alias boff="bluetoothctl power off"
-alias bon="bluetoothctl power on"
-
 set fish_greeting
 if status is-interactive
     # Commands to run in interactive sessions can go here
@@ -175,5 +119,6 @@ function ranger
 	return $return_value
 end
 
+cat ~/.config/alias.sh | source
 zoxide init fish | source
 starship init fish | source
